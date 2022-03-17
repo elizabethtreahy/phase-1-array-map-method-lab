24fc1b94f176295a7b1ffdb9ab614e5df2530521
Elizabeth <elizabethtreahy@gmail.com>
2022-03-17 18:20:34 -0400

    commit tree 714c29dd96a68e68c6e550aacf60d048e8cf8e6e

--- index.js
+++ index.js
@@ -11,6 +11,94 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((book) => {
+    const bookWords = book.split(" ");
+    const capitalizedBookWords = bookWords.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    const finalArray = capitalizedBookWords.join(" ");
+    return finalArray;
+  });
+};
+
+
+// function removeWhiteSpaceAndCapitalize(element) { 
+//   let completeString = ""
+//   const spaceSeparatedArray = element.split(" ") 
+//   const capitalizedSplitArray = spaceSeparatedArray.map((character) => character.charAt(0).toUpperCase() + character.slice(1))
+//   completeString = capitalizedSplitArray.join(" ")
+//   return completeString
+// }
+// titleCased(tutorials)
+
+
+
+
+
+
+
+
+
+// const titleCased = (inputArray) => {
+//   const outputArray = newArray(inputArray)
+//   return outputArray;
+// }
+
+// function newArray(valueString) {
+//   const completedStringArray = valueString.map(element => {
+//     removeWhiteSpaceAndCapitalize(element)
+//   })
+//   return splitArray
+// }
+
+// function removeWhiteSpaceAndCapitalize(element) { // 'what is JSON'
+//   let completeString = ""
+//   const spaceSeparatedArray = element.split("\s") //['what', 'is', 'JSON']
+//   const capitalizedSplitArray = spaceSeparatedArray.map((character) => character.charAt(0).toUpperCase() + character.slice(1)) //['What', 'Is', 'JSON']
+//   capitalizedSplitArray.map((value) => {completeString = `${completeSring} ${value}`}) // 'What Is JSON'
+//   return completeString
+// }
+
+// // .map
+
+
+
+// titleCased(tutorials)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function capitalize(tutorials, string) {
+//   return tutorials.map((character) => character.charAt(0) + character.slice(1));
+// }
+
+
+// function capitalize(word) {
+//   return word
+//     .split('')
+//     .map((letter, index) => index ? letter.toLowerCase() : letter.toUpperCase(),)
+//     .join('');
+// }
+
+// function getFirstLetters(str) {
+//   const firstLetters = str
+//     .split(' ')
+//     .map(word => word[0])
+//     .join('');
+
+//   return firstLetters;
+// }
